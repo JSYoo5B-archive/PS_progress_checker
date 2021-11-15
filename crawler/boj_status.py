b57eb094e026af88c125a13e3fc94ab60095bb9b
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-15 17:16:11 +0900

    feat: Write template code to parse status
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ crawler/boj_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_prob_status(username: str, prob_id = -1):
+    url = "https://www.acmicpc.net/status"
+    url += "?user_id={}&language_id=-1&result_id=-1&problem_id={}"
+    req_url = url.format(username, prob_id)
+    
+    # Request html data
+    req = requests.get(req_url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    stat_tbl = soup.select('#status-table > tbody:nth-child(2)')
+
+    print(stat_tbl)
