e9c054e8ba57ca9a2875861c9643ef051d57e97c
JaeSang Yoo <jsyoo5b@gmail.com>
2021-11-18 17:23:23 +0900

    feat: Add BOJ submit log struct
    * Holds information about submit log
    
    Signed-off-by: JaeSang Yoo <jsyoo5b@gmail.com>

---
+++ crawler/boj_submit_log.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from typing import List
+
+# BOJ submit tag order
+[ \
+        SUBMIT_ID, USERNAME, PROB_ID, PROB_NAME, \
+        RES_CODE, MEM_USED, ELAPSE_MS, LANG, \
+        SRC_LEN, SUBMIT_TIME \
+] = [ i for i in range(10) ]
+
+# BOJ result codes
+AC = 4 # 맞았습니다
+PE = 5 # 출력 형식이 잘못되었습니다
+WA = 6 # 틀렸습니다
+# TODO: add more result codes
+
+class Boj_submit_log():
+    def __init__(self, args):
+        self.s_id = args[SUBMIT_ID]
+        self.username = args[USERNAME]
+        self.prob_id = args[PROB_ID]
+        self.prob_name = args[PROB_NAME]
+        self.res_code = args[RES_CODE]
+        self.mem_used = args[MEM_USED]
+        self.elapsed_ms = args[ELAPSE_MS]
+        self.lang = args[LANG]
+        self.src_len = args[SRC_LEN]
+        self.submit_time = args[SUBMIT_TIME]
+
